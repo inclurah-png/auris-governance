7b2152e198bc8d10aa375e5bd1eb7b6a0d488d05
inclurah-png <inclurah@gmail.com>
2026-06-22 15:29:25 +0100

    Refactor index.js to use app from './app'
    Refactor index.js to use app from './app' instead of express.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,4 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    name: 'AURIS Governance API',
-    version: '1.0.0',
-    status: 'running'
-  });
-});
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
