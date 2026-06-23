360bdd3cac16788b3e48bbfde54271e26bbcf3a5
inclurah-png <inclurah@gmail.com>
2026-06-23 11:30:42 +0100

    Add Navigation component to Layout
    Update

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,9 +1,13 @@
+import Navigation from './Navigation';
+
 export default function Layout({ children }) {
   return (
     <div>
       <header>
         <h1>AURIS Governance</h1>
       </header>
+
+      <Navigation />
 
       <main>
         {children}
